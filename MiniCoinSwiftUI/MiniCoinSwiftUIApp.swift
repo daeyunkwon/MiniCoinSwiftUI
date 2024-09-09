93b4de62b6869d67ab64c97acaaa5aab295678a4
Daeyun <sweet_ray@naver.com>
2024-09-09 23:55:00 +0900

    Initial Commit

---
+++ MiniCoinSwiftUI/MiniCoinSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCoinSwiftUIApp.swift
+//  MiniCoinSwiftUI
+//
+//  Created by 권대윤 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniCoinSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
