190afe284fc5dfeaa2a9a73235959aa9d6db5988
Daeyun <sweet_ray@naver.com>
2024-09-10 23:28:17 +0900

    Feat: 코인 트렌딩 화면에서 Top15Coin 셀 구현

--- MiniCoinSwiftUI/Presentation/Trending/TrendingView.swift
+++ MiniCoinSwiftUI/Presentation/Trending/TrendingView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import Kingfisher
+
 struct TrendingView: View {
 
     //MARK: - Properties
@@ -18,6 +20,16 @@ struct TrendingView: View {
         case coin = "Top15 Coin"
         case nft = "Top7 NFT"
     }
+    
+    //목록을 1부터 1000까지 만듬
+    private let data = Array(1...1000).map { "목록 \($0)"}
+    
+    //화면을 그리드형식으로 꽉채워줌
+    private let layout = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
     
     //MARK: - Body
     
@@ -41,32 +53,26 @@ struct TrendingView: View {
                         }
                     }
                     
-                    
                     Text(Section.coin.rawValue)
                         .font(.title)
                         .bold()
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding()
                     ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack {
-                            ForEach(Section.allCases, id: \.self) { item in
-                                
+                        LazyHGrid(rows: layout, spacing: 20) {
+                            ForEach(1..<coinList.count) { i in
+                                top15CoinRowView(index: i)
                             }
                         }
+                        .padding(.horizontal)
                     }
                     
-                    Text(Section.nft.rawValue)
-                        .font(.title)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding()
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack {
-                            ForEach(Section.allCases, id: \.self) { item in
-                                favoriteRowView()
-                            }
-                        }
-                    }
+                    
+                    
+                    
+                    
+                    
+                    
                     
                     
                 }
@@ -135,6 +141,39 @@ struct TrendingView: View {
                 }
         }
         .padding(.horizontal, 20)
+    }
+    
+    private func top15CoinRowView(index i: Int) -> some View {
+        VStack {
+            HStack {
+                Text("\(i)")
+                    .font(.title)
+                    .bold()
+                KFImage.url(URL(string: coinList[i].item.small))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 25, height: 25)
+                
+                VStack(alignment: .leading) {
+                    Text(coinList[i].item.name)
+                        .font(.headline)
+                    Text(coinList[i].item.symbol)
+                        .font(.caption)
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    
+                    Text("\(coinList[i].item.data.price)")
+                        .font(.headline)
+                    Text("\(coinList[i].item.data.price_change_percentage_24h.krw)")
+                        .font(.caption)
+                }
+                
+            }
+            .padding()
+            .frame(width: 300)
+            Divider()
+        }
     }
 }
 
